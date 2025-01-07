253574a9eeb64644ddac0a7dbbafa189a910f4ab
NiTiSon <NiTiSon@users.noreply.github.com>
2025-01-07 17:12:31 +0300

    add benchmark

---
+++ benchmarks/MutableString.Benchmark/Program.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using NiTiS;
+using System;
+
+namespace Benchmark;
+
+public sealed class Program
+{
+	public static void Main(string[] args)
+	{
+		BenchmarkRunner.Run<Test>();
+	}
+}
+
+[SimpleJob(RuntimeMoniker.NativeAot90, baseline: true)]
+[SimpleJob(RuntimeMoniker.Net90)]
+[SimpleJob(RuntimeMoniker.Net60)]
+[MemoryDiagnoser]
+public class Test
+{
+
+}
